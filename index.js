8e2b1bdf87175d0896af07a08c46dd08c466bf20
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2022-09-06 15:35:42 +0200

    Newest Updates

--- index.js
+++
@@ -1,9 +0,0 @@
-function updateLabel() {
-  var addend1 = getNumber1();
-  console.log("addend1:", addend1);
-  var addend2 = getNumber2();
-  console.log("addend2:", addend2);
-  var sum = addend1 + addend2;
-  console.log("sum:", sum);
-  label.textContent = addend1 + " + " + addend2 + " = " + sum;
-}
---
+++ js/index.js
@@ -0,0 +1,56 @@
+// Debugging Lesson Task:
+
+// function updateLabel() {
+// var addend1 = getNumber1();
+// console.log("addend1:", addend1);
+// var addend2 = getNumber2();
+// console.log("addend2:", addend2);
+// var sum = addend1 + addend2;
+// console.log("sum:", sum);
+// label.textContent = addend1 + " + " + addend2 + " = " + sum;
+// }
+
+// JSDocs Lesson Task:
+
+/**
+ * Adds two numbers together
+ * @param {dataType} parameterName parameterDescription
+ * @param {dataType} parameterName parameterDescription
+ * @param {number} a First value
+ * @param {number} b Second value
+ * @returns {dataType} returnDescription
+ * @returns {number} Sum of params
+ * @example
+ * ```js
+ * // Adds two number together
+ * const a = 1;
+ * const b = 2;
+ * const sum = addNumbers(a, b);
+ * // except sum to be 3
+ * ```
+ */
+function addNumbers(a, b) {
+  const result = a + b;
+  return result;
+}
+
+// JSON Lesson Task
+
+// Template object
+const myCountry = {
+  country: "Norway",
+  city: "Oslo",
+  currency: "Krone",
+};
+
+const myCountryJSON = JSON.stringify(myCountry);
+
+console.log(myCountryJSON);
+
+
+const countryJSON = `{"country": "Norway", "city": "Oslo", "currency": "Krone"}`;
+
+const country = JSON.parse(countryJSON);
+
+console.log(country);
+
