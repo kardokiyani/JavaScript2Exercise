951cdbc44144e63ad03ea41e5b1be4d764ca8896
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2022-09-08 17:21:55 +0200

    Newest Updates

--- js/index.js
+++ js/index.js
@@ -73,3 +73,66 @@ localStorage.setItem("pet", jsonPet);
 const petRetrieved = localStorage.getItem("pet");
 const newPet = JSON.parse(petRetrieved);
 console.log(newPet);
+
+// Map Lesson Task
+
+const users = new Map();
+
+users.set(`12 `, { firstName: "Ola", lastName: "Nordmann" });
+
+console.log(users.size);
+
+users.set(`50`, { firstName: "Kari", lastName: "Nordmann" });
+
+console.log(users.size);
+
+users.delete(`12`);
+
+console.log(users.size);
+
+// Set Lesson Task
+
+const values = new Set();
+
+values.add(`1,2,2,2,3,4`);
+
+console.log(values.size);
+
+values.has(`1`);
+
+values.has(`a`);
+
+// Classes
+
+class Person {
+  language = `Norwegian`;
+  constructor(firstName, lastName) {
+    // Set 'firstName' to the 'firstName' parameter
+    this.firstName = firstName;
+    // Set 'lastName' to the 'lastName' parameter
+    this.lastName = lastName;
+  }
+
+  // 'speak' method that logs a greeting message
+  speak() {
+    console.log(`Hello ${this.firstName} ${this.lastName}`);
+  }
+}
+
+const newUser = new Person(`Ola`, `Nordmann`);
+// Logs "Hello Ola Nordmann!
+newUser.speak();
+
+class thePerson {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  speak() {
+    console.log(this.firstName, this.lastName);
+  }
+}
+
+const human = new thePerson(`Ola`, `Nordmann`);
+
+human.speak();
